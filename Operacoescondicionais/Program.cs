fe057f11424976d56366f6cef47d5e0ceded3398
JOSE AUGUSTO SILVA <jose.asilva49@senacsp.edu.br>
2018-04-19 10:26:30 -0300

    criação de operações condicionais

---
+++ Operacoescondicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operacoescondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
