9c4d70249403aca94ca74c132a73e8a56678493f
JOSE AUGUSTO SILVA <jose.asilva49@senacsp.edu.br>
2018-04-19 11:11:34 -0300

    Declaração de variáveis

--- Operacoescondicionais/Program.cs
+++ Operacoescondicionais/Program.cs
@@ -6,7 +6,16 @@ namespace Operacoescondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentário in-line(em linha)
+            /*
+            Comentário in-block(em bloco)
+            */
+            //Declaração da variável operação. Ela será do tipo inteiro
+            int operacao;
+
+            //Declaração das variáveis n1 e n2. elas serão 
+            double n1,n2;
+
         }
     }
 }
